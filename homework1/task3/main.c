ee15bed1dc2c10b20e72ddd79e9d17daf90a9bff
marina-popova11 <181482786+marina-popova11@users.noreply.github.com>
2024-10-19 16:57:14 +0300

    Fixed task3 in homework 1
    1. variableOfSign помогает изменить знак делимого или делителя, чтобы удобнее было посчитать неполное делимое;
    2. while (divisible * variableOfSign >= divider) { //умножаем на variableOfSign, чтобы делимое стало положительным(если оно отрицательное);
    3. divisible -= divider * variableOfSign; //если делитель изначально имел знак "-", мы его возвращаем(если и делитель, и делимое были отрицательными);

--- homework1/task3/main.c
+++ homework1/task3/main.c
@@ -2,50 +2,56 @@
 
 int division(int a, int b);
 
-void main() {
+int main() {
     int a;
     int b;
     printf("Enter the first number: \n");
     int result1 = scanf("%d", &a);
     printf("Enter the second number: \n");
     int result2 = scanf("%d", &b);
-    if (b == 0)
+    if (b == 0) {
         printf("ERROR");
-    else {
+        return 1;
+    } else {
         int result = 0;
         if (a != 0) {
             result = division(a, b);
         }
-
         printf("Result = %d\n", result);
     }
+    return 0;
 }
 
 int division(int a, int b) {
     int divisible = a;
+    int divider = b;
     int quotient = 0;
-    int d = 1;
-    int k = 1;
+    int signOfQuotient = 1;
+    int variableOfSign  = 1; 
 
     if (a > 0) {
-        d = 1;
+        signOfQuotient = 1;
         if (b < 0) {
-            d = -1;
-            b = -b;
+            signOfQuotient = -1;
+            divider = -divider;
         }
     }
     else if (a < 0) {
-        d = -1;
-        k = -1;
+        signOfQuotient = -1;
+        variableOfSign = -1;
         if (b < 0) {
-            d = 1;
-            b = -b;
+            signOfQuotient = 1;
+            divider = -divider;
         }
     }
-    while (divisible * k >= b) {
-        divisible -= b * k;
-        quotient += d;
+    while (divisible * variableOfSign >= divider) {
+        divisible -= divider * variableOfSign; 
+        quotient += signOfQuotient;
     }
+    if (a < 0 && b > 0) {
+        quotient -= 1;
+    } else if (a < 0 && b < 0)
+        quotient += 1;
 
     return quotient;
 }
