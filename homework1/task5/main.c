cb3d6ff58189726855ecb56e56c738430400fbf9
marina-popova11 <181482786+marina-popova11@users.noreply.github.com>
2024-10-27 10:00:04 +0300

    Fixed task 5

--- homework1/task5/main.c
+++ homework1/task5/main.c
@@ -1,21 +1,39 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
-void main() {
-    char symbols[] = "(())()())()";
-    int count = 0;
-    for (int i = 0; i < strlen(symbols); i++) {
-        if (symbols[i] == '(') {
-            count++;
-        }
-        else if (symbols[i] == ')') {
-            count--;
-        }
-    }
-    if ((count < 0) || (count > 0)) {
-        printf("Not a balance\n");
-    }
-    else {
+bool isBalance(char* array);
+
+int main(void) {
+    char symbols[20];
+    printf("Enter the string: ");
+    int result = scanf("%s", symbols);
+
+    if (isBalance(symbols)) {
         printf("It is a balance\n");
     }
+    else {
+        printf("Not a balance\n");
+    }
+    return 0;
+}
+
+bool isBalance(char *array) {
+    int count = 0;
+    for (int i = 0; i < strlen(array); i++) {
+        if (array[i] == '(') {
+            count++;
+        }
+        else if (array[i] == ')') {
+            count--;
+        }
+        if (count < 0) {
+            return false;
+        }
+    }
+    if (count != 0) {
+        return false;
+    }
+    return true;
+
 }
