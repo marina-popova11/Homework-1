462cca69145eed9187af658341f5fbf800cf8f9e
marina-popova11 <181482786+marina-popova11@users.noreply.github.com>
2024-11-09 19:28:47 +0300

    Fixed task1 and task2 from homework2

--- homework2/task 1/main.c
+++ homework2/task 1/main.c
@@ -6,75 +6,80 @@
 #define ARRAY_LENGTH 20
 
 void halfQsort(int* array, int left, int right);
-void print_array(int A[], int N);
+void printArray(int array[], int length);
 bool testCorrect();
 
 int main() {
-	if (!testCorrect()) {
-		printf("Tests failed\n");
-		return 0;
-	}
+    if (!testCorrect()) {
+        printf("Tests failed\n");
+        return -1;
+    }
 
-	srand(time(NULL));
+    srand(time(NULL));
 
-	int* array = malloc(ARRAY_LENGTH * sizeof(int));
-	if (array == NULL) {
-		printf("Failed to allicated array\n");
-		return 1;
-	}
+    int* array = malloc(ARRAY_LENGTH * sizeof(int));
+    if (array == NULL) {
+        printf("Failed to allocate memory\n");
+        return 1;
+    }
 
-	printf("The source array:\n");
-	int num;
-	for (int i = 0; i < ARRAY_LENGTH; ++i) {
-		num = rand() % 30 + 1;
-		array[i] = num;
-	}
-	print_array(array, ARRAY_LENGTH);
+    printf("The source array:\n");
+    for (int i = 0; i < ARRAY_LENGTH; ++i) {
+        array[i] = rand() % 30 + 1;
+    }
+    printArray(array, ARRAY_LENGTH);
 
-	halfQsort(array, 0, ARRAY_LENGTH - 1);
-	printf("The sorted array:\n");
-	print_array(array, ARRAY_LENGTH);
+    halfQsort(array, 0, ARRAY_LENGTH - 1);
+    printf("The sorted array:\n");
+    printArray(array, ARRAY_LENGTH);
 
-	free(array);
-	return 0;
+    free(array);
+    return 0;
 }
 
-void print_array(int A[], int N) {
-	printf("[");
-	for (int i = 0; i < N; ++i)
-		printf(" %d ", A[i]);
-	printf("]");
-	printf("\n");
+void printArray(int array[], int length) {
+    printf("[");
+    for (int i = 0; i < length; ++i) {
+        printf(" %d ", array[i]);
+    }
+    printf("]");
+    printf("\n");
 }
 
 void halfQsort(int* array, int left, int right) {
-	if (left >= right)
-		return;
+    if (left >= right) {
+        return;
+    }
 
-	int firstElement = array[0];
-	int i = left - 1;
-	int j = right + 1;
+    int firstElement = array[0];
+    int i = left - 1;
+    int j = right + 1;
 
-	while (i <= j) {
-		++i;
-		--j;
-		while (array[i] < firstElement) ++i;
-		while (array[j] >= firstElement) --j;
-		if (i <= j) {
-			int num = array[i];
-			array[i] = array[j];
-			array[j] = num;
-		}
-	}
+    while (i <= j) {
+        ++i;
+        --j;
+        while (array[i] < firstElement) {
+            ++i;
+        }
+        while (array[j] >= firstElement) {
+            --j;
+        }
+        if (i <= j) {
+            int num = array[i];
+            array[i] = array[j];
+            array[j] = num;
+        }
+    }
 }
 
 bool testCorrect() {
-	int array_2[ARRAY_LENGTH] = { 6, 7, 4, 5, 3, 6, 7, 2, 0, 4, 5, 2, 7, 6, 9, 11, 2, 13, 8, 3 };
-	int sortedArray_2[ARRAY_LENGTH] = { 3, 2, 4, 5, 3, 2, 5, 2, 0, 4, 7, 6, 7, 6, 9, 11, 7, 13, 8, 6 };
-	halfQsort(array_2, 0, ARRAY_LENGTH - 1);
-	for (int i = 0; i < ARRAY_LENGTH; ++i) {
-		if (array_2[i] != sortedArray_2[i])
-			return false;
-	}
-	return true;
+    int array[ARRAY_LENGTH] = { 6, 7, 4, 5, 3, 6, 7, 2, 0, 4, 5, 2, 7, 6, 9, 11, 2, 13, 8, 3 };
+    int sortedArray[ARRAY_LENGTH] = { 3, 2, 4, 5, 3, 2, 5, 2, 0, 4, 7, 6, 7, 6, 9, 11, 7, 13, 8, 6 };
+    halfQsort(array, 0, ARRAY_LENGTH - 1);
+    for (int i = 0; i < ARRAY_LENGTH; ++i) {
+        if (array[i] != sortedArray[i]) {
+            return false;
+        }
+    }
+    return true;
 }
--- homework2/task 2/main.c
+++ homework2/task 2/main.c
@@ -7,109 +7,123 @@
 #define ARRAY_LENGTH 100000
 #define LENGTH 15
 
-void Babble(int* array, int arrayLength);
-void Counting(int* array, int arrayLength);
-static void print_array(int* A, int N);
+bool babbleSorting(int* array, int arrayLength);
+bool countingSorting(int* array, int arrayLength);
+static void printArray(int* array, int length);
 static bool testCorrect();
 
 int main() {
-	if (!testCorrect()) {
-		printf("Tests failed\n");
-		return 0;
-	}
+    if (!testCorrect()) {
+        printf("Tests failed\n");
+        return -1;
+    }
 
-	srand(time(NULL));
+    srand(time(NULL));
 
-	int* array = malloc(ARRAY_LENGTH * sizeof(int));
-	if (array == NULL) {
-		printf("Failed to allicated array\n");
-		return 1;
-	}
-	int num;
-	for (int i = 0; i < ARRAY_LENGTH; ++i) {
-		num = rand() % 50;
-		array[i] = num;
-	}
+    int* array = malloc(ARRAY_LENGTH * sizeof(int));
+    if (array == NULL) {
+        printf("Failed to allicated array\n");
+        return 1;
+    }
+    int num;
+    for (int i = 0; i < ARRAY_LENGTH; ++i) {
+        num = -100 + rand() % 200;
+        array[i] = num;
+    }
 
-	clock_t startTimeCounting;
-	startTimeCounting = clock();
-	Counting(array, ARRAY_LENGTH);
+    clock_t startTimeBabble = clock();
+    babbleSorting(array, ARRAY_LENGTH);
 
-	double timeCounting = clock() - startTimeCounting;
-	printf("Processor time spent on sorting by counting: %f seconds\n", (float)timeCounting / CLOCKS_PER_SEC);
+    double timeBabble = clock() - startTimeBabble;
+    printf("Processor time spent on bubble sorting: %f seconds\n", (float)timeBabble / CLOCKS_PER_SEC);
+    clock_t startTimeCounting = clock();
+    countingSorting(array, ARRAY_LENGTH);
 
-	clock_t startTimeBabble;
-	startTimeBabble = clock();
-	Babble(array, ARRAY_LENGTH);
+    double timeCounting = clock() - startTimeCounting;
+    printf("Processor time spent on sorting by counting: %f seconds\n", (float)timeCounting / CLOCKS_PER_SEC);
+    free(array);
 
-	double timeBabble = clock() - startTimeBabble;
-	printf("Processor time spent on bubble sorting: %f seconds\n", (float)timeBabble / CLOCKS_PER_SEC);
-	free(array);
-
-	return 0;
+    return 0;
 }
 
-void Babble(int* array, int arrayLength) {
-	for (int i = 0; i < arrayLength - 1; ++i) {
-		for (int j = 0; j < arrayLength - 1 - i; ++j) {
-			if (array[j] > array[j + 1]) {
-				int num = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = num;
-			}
-
-		}
-
-	}
+bool babbleSorting(int* array, int arrayLength) {
+    for (int i = 0; i < arrayLength; ++i) {
+        bool flag = false;
+        for (int j = arrayLength - 1; j > i; --j) {
+            if (array[j - 1] > array[j]) {
+                flag = true;
+                int num = array[j - 1];
+                array[j - 1] = array[j];
+                array[j] = num;
+            }
+        }
+        if (!flag) {
+            break;
+        }
+    }
+    return true;
 }
 
-void Counting(int* array, int arrayLength) {
-	int maxElement = 0;
-	for (int i = 0; i < arrayLength; ++i)
-		if (array[i] > maxElement)
-			maxElement = array[i];
+bool countingSorting(int* array, int arrayLength) {
+    int maxElement = 0;
+    for (int i = 0; i < arrayLength; ++i) {
+        if (array[i] > maxElement) {
+            maxElement = array[i];
+        }
+    }
+    int minElement = 1000;
+    for (int i = 0; i < arrayLength; ++i) {
+        if (array[i] < minElement) {
+            minElement = array[i];
+        }
+    }
 
-	int* countArray = malloc((maxElement + 1) * sizeof(int));
-	if (countArray == NULL) {
-		printf("Failed to allicated array\n");
-		return 1;
-	}
-	memset(countArray, 0, (maxElement + 1) * sizeof(int));
+    int valuesCount = maxElement - minElement + 1;
+    int* countArray = calloc(valuesCount, sizeof(int));
+    if (countArray == NULL) {
+        printf("Failed to allocate memory\n");
+        return false;
+    }
 
-	for (int i = 0; i < arrayLength; ++i)
-		countArray[array[i]]++;
+    for (int i = 0; i < arrayLength; ++i) {
+        ++countArray[array[i] - minElement];
+    }
 
-	int pointer = 0;
-	for (int i = 0; i <= maxElement; ++i) {
-		for (int j = 0; j < countArray[i]; ++j) {
-			array[pointer++] = i;
-		}
-	}
-	free(countArray);
+    int pointer = 0;
+    for (int i = 0; i < valuesCount; ++i) {
+        for (int j = 0; j < countArray[i]; ++j) {
+            array[pointer] = i + minElement;
+            ++pointer;
+        }
+    }
+    free(countArray);
+    return true;
 }
 
-static void print_array(int* A, int N) {
-	printf("[");
-	for (int i = 0; i < N; i++)
-		printf(" %d ", A[i]);
-	printf("]");
-	printf("\n");
+static void printArray(int* array, int length) {
+    printf("[");
+    for (int i = 0; i < length; i++)
+        printf(" %d ", array[i]);
+    printf("]");
+    printf("\n");
 }
 
 static bool testCorrect() {
-	int array[LENGTH] = { 2, 14, 6, 0, 4, 5, 3, 7, 12, 8, 4, 5, 3, 2, 0 };
+    int array[LENGTH] = { 2, 14, 6, 0, 4, 5, 3, 7, 12, 8, 4, 5, 3, 2, 0 };
 
-	int sortedArray[LENGTH] = { 0, 0, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8, 12, 14 };
+    int sortedArray[LENGTH] = { 0, 0, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8, 12, 14 };
+    
+    babbleSorting(array, LENGTH);
+    for (int i = 0; i < LENGTH; ++i) {
+        if (array[i] != sortedArray[i])
+            return false;
+    }
 
-	Counting(array, LENGTH);
-	for (int i = 0; i < LENGTH; ++i) {
-		if (array[i] != sortedArray[i])
-			return false;
-	}
-	Babble(array, LENGTH);
-	for (int i = 0; i < LENGTH; ++i) {
-		if (array[i] != sortedArray[i])
-			return false;
-	}
-	return true;
+    countingSorting(array, LENGTH);
+    for (int i = 0; i < LENGTH; ++i) {
+        if (array[i] != sortedArray[i])
+            return false;
+    }
+    
+    return true;
 }
