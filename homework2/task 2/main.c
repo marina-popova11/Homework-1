d997da7d672b146994331478a7e66479bb2eac8e
marina-popova11 <181482786+marina-popova11@users.noreply.github.com>
2024-12-22 12:42:08 +0300

    Fixed task2 from homework2

--- homework2/task 2/main.c
+++ homework2/task 2/main.c
@@ -2,17 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 #include <time.h>
 
 #define ARRAY_LENGTH 100000
 #define LENGTH 15
 
-bool babbleSorting(int* array, int arrayLength);
-bool countingSorting(int* array, int arrayLength);
+bool babbleSort(int* array, int arrayLength);
+bool countingSort(int* array, int arrayLength);
 static void printArray(int* array, int length);
 static bool testCorrect();
 
-int main() {
+int main(void) {
     if (!testCorrect()) {
         printf("Tests failed\n");
         return -1;
@@ -20,51 +21,65 @@ int main() {
 
     srand(time(NULL));
 
-    int* array = malloc(ARRAY_LENGTH * sizeof(int));
-    if (array == NULL) {
+    int* arrayForBubble = malloc(ARRAY_LENGTH * sizeof(int));
+    if (arrayForBubble == NULL) {
         printf("Failed to allicated array\n");
-        return 1;
+        return -1;
     }
-    int num;
+    int num1 = 0;
     for (int i = 0; i < ARRAY_LENGTH; ++i) {
-        num = -100 + rand() % 200;
-        array[i] = num;
+        num1 = -100 + rand() % 200;
+        arrayForBubble[i] = num1;
     }
-
     clock_t startTimeBabble = clock();
-    babbleSorting(array, ARRAY_LENGTH);
+    babbleSort(arrayForBubble, ARRAY_LENGTH);
+    clock_t endTimeBubble = clock();
 
-    double timeBabble = clock() - startTimeBabble;
+    double timeBabble = endTimeBubble - startTimeBabble;
     printf("Processor time spent on bubble sorting: %f seconds\n", (float)timeBabble / CLOCKS_PER_SEC);
+    
+    int* arrayForCounting = malloc(ARRAY_LENGTH * sizeof(int));
+    if (arrayForCounting == NULL) {
+        printf("Failed to allicated array\n");
+        return -1;
+    }
+    int num2 = 0;
+    for (int i = 0; i < ARRAY_LENGTH; ++i) {
+        num2 = -100 + rand() % 200;
+        arrayForCounting[i] = num2;
+    }
     clock_t startTimeCounting = clock();
-    countingSorting(array, ARRAY_LENGTH);
+    countingSort(arrayForCounting, ARRAY_LENGTH);
+    clock_t endTimeCounting = clock();
 
-    double timeCounting = clock() - startTimeCounting;
+    double timeCounting = endTimeCounting - startTimeCounting;
     printf("Processor time spent on sorting by counting: %f seconds\n", (float)timeCounting / CLOCKS_PER_SEC);
-    free(array);
+    free(arrayForBubble);
+    free(arrayForCounting);
 
     return 0;
 }
 
-bool babbleSorting(int* array, int arrayLength) {
-    for (int i = 0; i < arrayLength; ++i) {
-        bool flag = false;
-        for (int j = arrayLength - 1; j > i; --j) {
-            if (array[j - 1] > array[j]) {
+bool babbleSort(int* array, int arrayLength) {
+    bool flag = false;
+    while (arrayLength > 1) {
+        for (int i = 0; i < arrayLength - 1; ++i) {
+            if (array[i] > array[i + 1]) {
                 flag = true;
-                int num = array[j - 1];
-                array[j - 1] = array[j];
-                array[j] = num;
+                int num = array[i + 1];
+                array[i + 1] = array[i];
+                array[i] = num;
             }
         }
         if (!flag) {
             break;
         }
+        --arrayLength;
     }
     return true;
 }
 
-bool countingSorting(int* array, int arrayLength) {
+bool countingSort(int* array, int arrayLength) {
     int maxElement = 0;
     for (int i = 0; i < arrayLength; ++i) {
         if (array[i] > maxElement) {
@@ -109,21 +124,24 @@ static void printArray(int* array, int length) {
 }
 
 static bool testCorrect() {
-    int array[LENGTH] = { 2, 14, 6, 0, 4, 5, 3, 7, 12, 8, 4, 5, 3, 2, 0 };
+    int array1[LENGTH] = { 2, 14, 6, 0, 4, 5, 3, 7, 12, 8, 4, 5, 3, 2, 0 };
 
-    int sortedArray[LENGTH] = { 0, 0, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8, 12, 14 };
-    
-    babbleSorting(array, LENGTH);
+    int sortedArray1[LENGTH] = { 0, 0, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8, 12, 14 };
+
+    babbleSort(array1, LENGTH);
     for (int i = 0; i < LENGTH; ++i) {
-        if (array[i] != sortedArray[i])
+        if (array1[i] != sortedArray1[i])
             return false;
     }
 
-    countingSorting(array, LENGTH);
+    int array2[LENGTH] = { 2, 14, 6, -1, 4, 5, 3, 7, 12, 8, 4, 5, 3, 2, 0 };
+    int sortedArray2[LENGTH] = { -1, 0, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8, 12, 14 };
+
+    countingSort(array2, LENGTH);
     for (int i = 0; i < LENGTH; ++i) {
-        if (array[i] != sortedArray[i])
+        if (array2[i] != sortedArray2[i])
             return false;
     }
-    
+
     return true;
 }
