14b4ac1709334d7c540884f2b809ad60731af764
marina-popova11 <181482786+marina-popova11@users.noreply.github.com>
2024-12-23 16:01:21 +0300

    Added explanations to the functions

--- homework6/List/list.h
+++ homework6/List/list.h
@@ -10,20 +10,29 @@ typedef ListElement* Position;
 
 typedef int Value;
 
+// create list
 List* createList(void);
 
+// adding new element in list by position
 bool addElement(List* list, Position position, Value value);
 
+// deleting element from list by position
 bool deleteElement(List* list, Position position);
 
+// getting value from list by position
 Value getValue(List* list, Position position);
 
+//changing the value
 void setValue(List* list, Position position, Value value);
 
+//assigning the first position
 Position first(List* list);
 
+//assigning the next position
 Position next(Position position);
 
+//checking for the last element
 bool isLast(List* list, Position position);
 
-bool isValid(List * list, Position position);
+//checking for the existence of a value
+bool isValid(List* list, Position position);
--- homework6/task2/list.h
+++ homework6/task2/list.h
@@ -4,14 +4,20 @@ typedef struct ListElement ListElement;
 
 typedef struct CircularList CircularList;
 
+// create circular list
 CircularList* createCircularList(int number);
 
+//getting next element
 ListElement* getNext(ListElement* element);
 
+// getting element`s value
 int getValue(ListElement* element);
 
+// deleting element from list
 void deleteElement(CircularList* list, ListElement* current, ListElement* previous);
 
+// getting head element from list
 CircularList* getHead(CircularList* list);
 
+// getting tail element from list
 CircularList* getTail(CircularList* list);
